d393dcef50c9d4ed2fcb72c6e393b94f63483fb4
wujiaqi <1584486043@qq.com>
2020-02-27 14:59:45 +0800

    filter

---
+++ xiangmu/01-spring-boot-filter/src/main/java/com/bjsxt/App.java
@@ -0,0 +1,15 @@
+package com.bjsxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication 
+@ServletComponentScan //在 springBoot 启动时会扫描@WebServlet，并将该类实例 化 
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+		
+	}
+
+}
